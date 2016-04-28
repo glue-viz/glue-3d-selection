0c2cc6f0fa1ef813ed915f8f66eae9d5045bde05
Penny Qian <qianxuran@nao.cas.cn>
2016-04-28 14:52:13 -0400

    Revert "3dfloodfill with Cython"

--- flood_fill/flood_fill.pyx
+++ flood_fill/flood_fill.pyx
@@ -1,7 +1,8 @@
 import numpy as np
-cimport numpy as np
-# def cyfill(float[:, :, :] data,   #np.dtype('>f4')
-def cyfill(np.ndarray[np.float64_t, ndim=3] data, tuple start_coords, Py_ssize_t fill_value, float threshold):
+cimport numpy as cnp
+
+def cyfill(unsigned char[:, ::1] data, tuple start_coords,
+           Py_ssize_t fill_value, float threshold):
     """
     Flood fill algorithm
     
@@ -19,18 +20,13 @@ def cyfill(np.ndarray[np.float64_t, ndim=3] data, tuple start_coords, Py_ssize_t
     None, ``data`` is modified inplace.
     """
     cdef:
-        Py_ssize_t x, y, z, xsize, ysize, zsize, orig_value
-        float thres 
+        Py_ssize_t x, y, xsize, ysize, orig_value
+        float thres
         set stack
     
     xsize = data.shape[0]
     ysize = data.shape[1]
-    zsize = data.shape[2]
-    
-    print data.shape[0]
-    print data[20, 20, 20]
-    orig_value = int(data[start_coords[0], start_coords[1], start_coords[2]])
-    # orig_value = data[20, 20, 20]
+    orig_value = data[start_coords[0], start_coords[1]]
 	# this threshold should combine the relation between dragging distance and greyscale(0~255)
     thres = threshold
     if fill_value == orig_value:
@@ -38,23 +34,19 @@ def cyfill(np.ndarray[np.float64_t, ndim=3] data, tuple start_coords, Py_ssize_t
                          "already present is unsupported. "
                          "Did you already fill this region?")
     
-    stack = set(((start_coords[0], start_coords[1], start_coords[2]),))
+    stack = set(((start_coords[0], start_coords[1]),))
 
     while stack:
-        x, y, z = stack.pop()
+        x, y = stack.pop()
 		# set the threshold as 30 here but this could be get through an input
-        if data[x, y, z]>orig_value-thres and data[x, y, z]<orig_value+thres:
-            data[x, y, z] = fill_value
+        if data[x, y]>orig_value-thres and data[x, y]<orig_value+thres:
+            data[x, y] = fill_value
             if x > 0:
-                stack.add((x - 1, y, z))
+                stack.add((x - 1, y))
             if x < (xsize - 1):
-                stack.add((x + 1, y, z))
+                stack.add((x + 1, y))
             if y > 0:
-                stack.add((x, y - 1, z))
+                stack.add((x, y - 1))
             if y < (ysize - 1):
-                stack.add((x, y + 1, z))
-            if z > 0:
-                stack.add((x, y, z - 1))
-            if z < (zsize - 1):
-                stack.add((x, y, z + 1))
+                stack.add((x, y + 1))
     return data
--- floodfill_selection/multivol/__init__.py
+++
@@ -1,3 +0,0 @@
-from .rgba_volume_visual import RGBAVolume
-from .multi_volume_visual import MultiVolume
-from .colors import get_translucent_cmap
